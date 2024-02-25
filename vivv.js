03057b82e121813cb37d6e5475bdacd72668a65f
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-02-25 10:47:49 +0800

    feat: add picocolors

--- vivv.js
+++ vivv.js
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import http from "http";
+import color from "picocolors";
+
+http
+  .createServer((req, res) => {
+    res.end("ho");
+  })
+  .listen(5173);
+
+console.log(`${color.red("hihi")} server on`);
