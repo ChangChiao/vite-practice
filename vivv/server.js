601a48614f05a48435ddd9fb141abb71e456bae0
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-02-25 11:30:42 +0800

    feat: add websocket

--- vivv/server.js
+++ vivv/server.js
@@ -1,15 +1,27 @@
 import http from "http";
 import color from "picocolors";
 import connect from "connect";
+import { WebSocketServer } from "ws";
 import { indexHTMLMiddleware } from "./middlewares";
 
+const { PROJECT_NAME, PORT_HTTP, PORT_WS } = process.env;
+
+const createWSServer = () => {
+  const server = new WebSocketServer({ port: PORT_WS });
+  server.on("connection", (ws) => {
+    console.log(`${color.green("ws connect")}`);
+    ws.on("message", (data) => {
+      console.log("Received: %s", data);
+    });
+  });
+};
+
 const middleware = connect();
-
-const { PROJECT_NAME, PORT_HTTP } = process.env;
-
 middleware.use(indexHTMLMiddleware);
 const createServer = () => {
   http.createServer(middleware).listen(PORT_HTTP);
+
+  createWSServer();
 
   console.log(
     `${color.red(PROJECT_NAME)} server on ${color.green(
