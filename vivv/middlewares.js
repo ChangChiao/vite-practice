8fbb99f50882d4f99363d82d0ff5c4457e13f2c3
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-02-25 11:43:52 +0800

    feat: add getFilePath function

--- vivv/middlewares.js
+++ vivv/middlewares.js
@@ -1,8 +1,38 @@
+import path from "path";
+
+const ContentTypes = {
+  html: "text/html",
+  js: "text/javascript",
+  css: "text/css",
+  png: "image/png",
+  jpg: "image/jpg",
+};
+
+const getFilePathAndContentType = (filename) => {
+  if (filename === "/") {
+    filename = "index.html";
+  }
+
+  const extname = path.extname(filename).replace(".", "");
+  const contentType = ContentTypes[extname] || "text/html";
+  const rootPath = process.cwd();
+  const filePath = path.join(rootPath, filename);
+
+  return { filePath, contentType };
+};
+
 const indexHTMLMiddleware = async (req, res) => {
-  const file = Bun.file("./index.html");
-  const content = await file.text();
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(content);
+  const { filePath, contentType } = getFilePathAndContentType(req.url);
+
+  try {
+    const file = Bun.file(filePath);
+    const content = await file.text();
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(content);
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("You have to create an index.html");
+  }
 };
 
 export { indexHTMLMiddleware };
