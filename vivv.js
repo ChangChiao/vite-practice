f39128a91cfb1b10357ab80ea107c98328fdbdf0
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-02-25 10:53:09 +0800

    chore: add env example

--- vivv.js
+++ vivv.js
@@ -1,10 +1,16 @@
 import http from "http";
 import color from "picocolors";
 
+const { PROJECT_NAME, PORT_HTTP } = process.env;
+
 http
   .createServer((req, res) => {
     res.end("ho");
   })
-  .listen(5173);
+  .listen(PORT_HTTP);
 
-console.log(`${color.red("hihi")} server on`);
+console.log(
+  `${color.red(PROJECT_NAME)} server on ${color.green(
+    `http://localhost:${PORT_HTTP}`
+  )}`
+);
