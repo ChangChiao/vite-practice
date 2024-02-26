97aaeb014450c7dbf50ed8a392cb19dc86635c00
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-02-26 20:46:19 +0800

    feat: add replaceImportMiddleware middleware

---
+++ app.js
@@ -0,0 +1,4 @@
+import trimEnd from "lodash-es/trimEnd.js";
+
+console.log("hello world");
+console.log("trimEnd", trimEnd);
--- vivv/server.js
+++ vivv/server.js
@@ -2,7 +2,7 @@ import http from "http";
 import color from "picocolors";
 import connect from "connect";
 import { WebSocketServer } from "ws";
-import { indexHTMLMiddleware } from "./middlewares";
+import { indexHTMLMiddleware, replaceImportMiddleware } from "./middlewares";
 
 const { PROJECT_NAME, PORT_HTTP, PORT_WS } = process.env;
 
@@ -21,6 +21,7 @@ const createWSServer = () => {
 };
 
 const middleware = connect();
+middleware.use(replaceImportMiddleware);
 middleware.use(indexHTMLMiddleware);
 const createServer = () => {
   http.createServer(middleware).listen(PORT_HTTP);
