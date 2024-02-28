38612a32a3d36c14df66945263c9feed01e61403
joe <JoeCT_Chang@moxa.com>
2024-02-28 16:17:10 +0800

    refactor: add middleware folder

---
+++ vivv/middlewares/index_html.js
@@ -0,0 +1,28 @@
+import path from "path";
+
+const indexHTMLMiddleware = async (req, res) => {
+  const { filePath, contentType } = getFilePathAndContentType(req.url);
+
+  try {
+    const file = Bun.file(filePath);
+    let content = await file.text();
+
+    if (path.basename(filePath) === "index.html") {
+      const regex = /(<head>)([\s\S]*?<\/head>)/i;
+      const match = content.match(regex);
+      const clientScript = "<script src='vivv/client.js'></script>";
+
+      if (match) {
+        content = content.replace(match[0], match[1] + clientScript + match[2]);
+      }
+    }
+
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(content);
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("You have to create an index.html");
+  }
+};
+
+export { indexHTMLMiddleware };
--- vivv/middlewares/replace_import.js
+++ vivv/middlewares/replace_import.js
@@ -37,29 +37,4 @@ const replaceImportMiddleware = async (req, res, next) => {
   next();
 };
 
-const indexHTMLMiddleware = async (req, res) => {
-  const { filePath, contentType } = getFilePathAndContentType(req.url);
-
-  try {
-    const file = Bun.file(filePath);
-    let content = await file.text();
-
-    if (path.basename(filePath) === "index.html") {
-      const regex = /(<head>)([\s\S]*?<\/head>)/i;
-      const match = content.match(regex);
-      const clientScript = "<script src='vivv/client.js'></script>";
-
-      if (match) {
-        content = content.replace(match[0], match[1] + clientScript + match[2]);
-      }
-    }
-
-    res.writeHead(200, { "Content-Type": contentType });
-    res.end(content);
-  } catch (error) {
-    res.writeHead(500, { "Content-Type": "text/plain" });
-    res.end("You have to create an index.html");
-  }
-};
-
-export { indexHTMLMiddleware, replaceImportMiddleware };
+export { replaceImportMiddleware };
