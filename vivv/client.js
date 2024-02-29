07ca6f5acf994038de278dea3e5461586e2d4708
joe <JoeCT_Chang@moxa.com>
2024-02-29 20:16:27 +0800

    feat: add chokidar

--- vivv/client.js
+++ vivv/client.js
@@ -2,7 +2,36 @@ const ws = new WebSocket("ws:/localhost:9527");
 
 ws.addEventListener("open", ({ target: s }) => {
   s.addEventListener("message", ({ data }) => {
-    console.log(data);
+    const result = JSON.parse(data);
+
+    switch (result.type) {
+      case "message":
+        console.log(result.content);
+        break;
+      case "change":
+        const { file } = result;
+        if (file.endsWith(".css")) {
+          fetch(file)
+            .then((data) => data.text())
+            .then((content) => {
+              const el = document.querySelector("style#hot");
+              if (el) {
+                el.textContent = content;
+              } else {
+                const el = document.createElement("style");
+                el.id = "hot";
+                el.textContent = content;
+                document.querySelector("head").appendChild(el);
+              }
+              console.log(content);
+            })
+            .catch((err) => console.log("err", err));
+        } else {
+          window.location.reload();
+        }
+        break;
+    }
+    console.log(result);
   });
 
   s.addEventListener("close", () => {
--- vivv/server.js
+++ vivv/server.js
@@ -3,14 +3,28 @@ import color from "picocolors";
 import connect from "connect";
 import { WebSocketServer } from "ws";
 import { indexHTMLMiddleware, replaceImportMiddleware } from "./middlewares";
+import chokidar from chokidar;
+import { getRelativePath } from "./utils";
 
 const { PROJECT_NAME, PORT_HTTP, PORT_WS } = process.env;
+const WATCH_LIST = ['index.html','src/*.js', 'src/*.css']
 
 const createWSServer = () => {
   const server = new WebSocketServer({ port: PORT_WS });
   server.on("connection", (ws) => {
     console.log(`${color.green("ws connect")}`);
-    ws.send(PROJECT_NAME + "connected");
+    ws.send({type: "message", content: `${PROJECT_NAME} Connected`});
+
+    //watcher
+    const watcher = chokidar.watcher(WATCH_LIST)
+    watcher.on('change', file => {
+      const messageObj = {
+        type: 'change',
+        file: getRelativePath(file),
+      }
+      ws.send(JSON.stringify(messageObj))
+    })
+
     ws.on("message", (data) => {
       console.log("Received: %s", data);
     });
